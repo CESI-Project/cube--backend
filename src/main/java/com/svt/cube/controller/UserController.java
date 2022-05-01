0dc66e89540389a55d70f97274b9628be16f8262
Gadiffet <theo.gadiffet@gmail.com>
2022-05-01 17:29:13 +0200

    feat(user): add controller userbyid

--- src/main/java/com/svt/cube/controller/UserController.java
+++ src/main/java/com/svt/cube/controller/UserController.java
@@ -5,14 +5,11 @@ import com.svt.cube.repository.RoleRepository;
 import com.svt.cube.repository.UserRepository;
 import com.svt.cube.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
@@ -56,6 +53,12 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public Optional<User> getUser(@PathVariable Long id) {
+        return userService.getUser(id);
     }
 
     @CrossOrigin
