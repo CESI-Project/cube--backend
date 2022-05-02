4708908f19d3c9abf58552aafebe5e55cf1cdceb
Gadiffet <theo.gadiffet@gmail.com>
2022-05-02 07:26:14 +0200

    fix(favorite): fix crossorigin error

--- src/main/java/com/svt/cube/controller/FavoriteController.java
+++ src/main/java/com/svt/cube/controller/FavoriteController.java
@@ -1,23 +1,14 @@
 package com.svt.cube.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
 import com.svt.cube.entity.Favorite;
 import com.svt.cube.payload.response.MessageResponse;
 import com.svt.cube.service.FavoriteService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/favorite")
@@ -30,7 +21,7 @@ public class FavoriteController {
     }
 
     @CrossOrigin
-    @GetMapping("/{id}/hisFavorites")
+    @GetMapping("{id}")
     // @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_MODERATOR') or
     // hasRole('ROLE_ADMIN') or hasRole('ROLE_SUPERADMIN')")
     public List<Favorite> getAllFavorites(@Valid @PathVariable Long id) {
