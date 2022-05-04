adfd5c7db0dbb619608eaa46c1d7d94b20281fa2
System Administrator <valentin.lacour@viacesi.fr>
2022-05-04 10:07:36 +0200

    dev

--- src/main/java/com/svt/cube/entity/View.java
+++ src/main/java/com/svt/cube/entity/View.java
@@ -8,10 +8,13 @@ public class View {
     @SequenceGenerator(name = "favorite_sequence", sequenceName = "favorite_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "favorite_sequence")
     private Integer id;
-    @Column(nullable = false, length = 3)
+    @Column(nullable = true, length = 3)
     private Integer topicId;
     @Column(length = 3)
     private Integer userId;
+
+    public View() {
+    }
 
     public View(Integer topicId, Integer userId) {
         this.topicId = topicId;
